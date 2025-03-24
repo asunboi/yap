f0abb2829bb92f1070338b5f470a2be2c3d30550
Alexander Sun <asun@scripps.edu>
2025-03-24 16:23:53 -0700

    restored directory structure after moving

---
+++ pipeline2_yap_s5/demultiplex_chrG.sbatch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=40000
+#SBATCH --time=12:00:00
+#SBATCH --partition=highmem
+#SBATCH -o /gpfs/home/asun/jin_lab/yap/logs/%x.o%j       # Name of stdout output file
+#SBATCH -e /gpfs/home/asun/jin_lab/yap/logs/%x.e%j 
+
+yap demultiplex \
+--fastq_pattern "/gpfs/home/asun/jin_lab/yap/raw_data/S_5/*.fq.gz" \
+-o /gpfs/home/asun/jin_lab/yap/pipeline2_yap_s5/yap_demultiplex \
+-j 16 \
+--aligner bismark \
+--config_path /gpfs/home/asun/jin_lab/yap/pipeline2_yap_s5/mct_config.ini
