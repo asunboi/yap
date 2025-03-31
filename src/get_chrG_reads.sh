9579e95e378bf901d043fd8482a680adc26b86c5
Alexander Sun <asun@scripps.edu>
2025-03-31 16:38:41 -0700

    testing local aligner and removing TSO from ecker trimming gh-10

--- src/get_chrG_reads.sh
+++ src/get_chrG_reads.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for d in /gpfs/home/asun/jin_lab/yap/pipeline2_yap_iseq0328/yap_demultiplex/iseq0328*/rna_bam/; do
+for d in /gpfs/home/asun/jin_lab/yap/pipeline2_yap_iseq0328_2/demultiplex/iseq0328*/rna_bam/; do
     echo $d
     cd $d
     bedtools intersect -a TotalRNAAligned.rna_reads.bam -b /gpfs/home/asun/jin_lab/yap/raw_data/ref_w_chrG/build/chrG.bed > intersect.bam
-    samtools view --no-header intersect.bam >> /gpfs/home/asun/jin_lab/yap/pipeline2_yap_iseq0328/TotalAlignedChrG.rna_reads.sam
+    samtools view --no-header intersect.bam >> /gpfs/home/asun/jin_lab/yap/pipeline2_yap_iseq0328_2/TotalAlignedChrG.rna_reads.sam
     rm intersect.bam
 done
