b17f9451d83caf4d5e2bb137517232c20b7a96c8
Alexander Sun <asun@scripps.edu>
2025-04-03 15:27:52 -0700

    bt2 aligned r and s5, cleaned up notebook for processing gh-15 gh-16 gh-10

--- pipeline2_yap_s5/generate_config_chrG.sh
+++ pipeline2_yap_s5/generate_config_chrG.sh
@@ -3,8 +3,8 @@
 yap default-mapping-config \
 --mode mct \
 --barcode_version V2 \
---bismark_ref "~/jin_lab/yap/raw_data/ref_w_chrG" \
---genome "~/jin_lab/yap/raw_data/ref_w_chrG/mm10_no_alt_ENCODE_chrL_chrG.fa" \
---chrom_size_path "~/jin_lab/yap/raw_data/ref_w_chrG/mm10_no_alt_ENCODE_chrL_chrG.sizes" \
---gtf "~/jin_lab/yap/raw_data/ref_w_chrG/mm10_no_alt_ENCODE_chrL_chrG.gtf" \
---star_ref "~/jin_lab/yap/raw_data/ref_w_chrG/star" > mct_config.ini
+--bismark_ref "/gpfs/home/asun/jin_lab/yap/raw_data/ref_w_chrG" \
+--genome "/gpfs/home/asun/jin_lab/yap/raw_data/ref_w_chrG/mm10_no_alt_ENCODE_chrL_chrG.fa" \
+--chrom_size_path "/gpfs/home/asun/jin_lab/yap/raw_data/ref_w_chrG/mm10_no_alt_ENCODE_chrL_chrG.sizes" \
+--gtf "/gpfs/home/asun/jin_lab/yap/raw_data/ref_w_chrG/mm10_no_alt_ENCODE_chrL_chrG.gtf" \
+--star_ref "/gpfs/home/asun/jin_lab/yap/raw_data/ref_w_chrG/star" > mct_config.ini
--- pipeline2_yap_s5/generate_summary.sh
+++ pipeline2_yap_s5/generate_summary.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yap summary -o ./yap_demultiplex
+yap summary -o ./yap_demultiplex -m mct
